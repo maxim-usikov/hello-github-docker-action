fae28f9379c22be7f5002fc756191360f7982d44
Maxim Usikov <mu.usikov@gmail.com>
2021-02-19 18:46:14 +0300

    chore: next step

--- .bin/docker_push.sh
+++ .bin/docker_push.sh
@@ -11,10 +11,6 @@ set -eou pipefail
 # IMAGE_NAME="hello-github-docker-action"
 # IMAGE_TAG="v1.0.0-beta.0"
 
-printf "${DOCKER_PASSWORD}" | docker login "${REGISTRY}" \
-  --username "${DOCKER_USERNAME}" \
-  --password-stdin
-
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${REGISTRY}/${OWNER}/${IMAGE_NAME}:${IMAGE_TAG}"
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${REGISTRY}/${OWNER}/${IMAGE_NAME}:latest"
 
