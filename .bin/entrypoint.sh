ede48cf5896dc4670ea550226e4d8cbc7eae1dca
Maxim Usikov <mu.usikov@gmail.com>
2021-02-20 16:13:13 +0300

    fix: remove "debug"

--- .bin/entrypoint.sh
+++ .bin/entrypoint.sh
@@ -6,11 +6,6 @@ COMMITLINT_HOME="/commitlint"
 COMMITLINT_CONFIG="${COMMITLINT_HOME}/commitlint.config.js"
 PATH="${COMMITLINT_HOME}/node_modules/.bin:${PATH}"
 
-pwd
-ls -la
-ls -la /
-ls -la "${COMMITLINT_HOME}"
-
 COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
 
 echo "${COMMIT_MESSAGE}" | commitlint --config "${COMMITLINT_CONFIG}" --verbose
