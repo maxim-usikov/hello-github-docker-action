6317ad33ff1c9ca35a0fa6b41ce3aa66813f8eb0
Maxim Usikov <mu.usikov@gmail.com>
2021-02-20 16:03:03 +0300

    chore: next step

--- .bin/entrypoint.sh
+++ .bin/entrypoint.sh
@@ -2,12 +2,15 @@
 
 set -eou pipefail
 
-PATH="node_modules/.bin:${PATH}"
+COMMITLINT_HOME="/commitlint"
+COMMITLINT_CONFIG="${COMMITLINT_HOME}/commitlint.config.js"
+PATH="${COMMITLINT_HOME}/node_modules/.bin:${PATH}"
 
+pwd
 ls -la
 ls -la /
+ls -la "${COMMITLINT_HOME}"
 
-COMMITLINT_CONFIG="./commitlint.config.js"
 COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
 
 echo "${COMMIT_MESSAGE}" | commitlint --config "${COMMITLINT_CONFIG}" --verbose
