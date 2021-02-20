60a3cb72ce08fdf72fe1daa3546b9968d6439c55
Maxim Usikov <mu.usikov@gmail.com>
2021-02-20 15:42:14 +0300

    chore: next step

--- .bin/entrypoint.sh
+++ .bin/entrypoint.sh
@@ -1,3 +1,12 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-printf "Let's begin"
+set -eou pipefail
+
+PATH="node_modules/.bin:${PATH}"
+
+ls -la
+
+COMMITLINT_CONFIG="./commitlint.config.js"
+COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
+
+echo "${COMMIT_MESSAGE}" | commitlint --config "${COMMITLINT_CONFIG}" --verbose
